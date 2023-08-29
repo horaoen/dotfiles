b941b0c5bdd7066d9c69f5db3ca59793d77552ef
horaoen <horaoen@gmail.com>
2023-08-29 11:44:12 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # submodule init
 git submodule init
 git submodule update
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+set_soft_link() {
+	left_file=$1
+	right_file=$2
 
-if [[ ! -e ~/.zshrc ]]; then
-	ln -s "$BASEDIR/zshrc" ~/\.zshrc
-	echo "~/.zshrc created."
-else
-	echo "File .zshrc already exists. Skipping symbolic link creation."
-fi
+	if [[ -e $right_file ]]; then
+		echo "File $right_file already exists."
+		read -p "Do you want to delete it? (y/n) " choice
+		if [[ "$choice" == "y" ]]; then
+			rm -rf $right_file
+			ln -s $left_file $right_file
+			echo "$right_file Symbolic link created."
+		else
+			echo "Aborted."
+		fi
+	else
+		ln -s $left_file $right_file
+		echo "$right_file Symbolic link created."
+	fi
+}
+
+# zshrc
+set_soft_link "$BASEDIR/zshrc" ~/.zshrc
+
+set_soft_link "$BASEDIR/nvim" ~/.config/nvim
